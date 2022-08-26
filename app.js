2155fac4f16938b931fa9cae7b306bf11cf1c819
Farshad Mahmoudifar <fmahmoudifar@gmail.com>
2022-08-26 09:37:35 +0300

    add 18

--- app.js
+++ app.js
@@ -1,13 +1,7 @@
-const EventEmitter = require('events')
+const { createReadStream } = require('fs')
 
-const customEmitter = new EventEmitter()
+const stream = createReadStream('./content/big.txt')
 
-customEmitter.on('response', (name, id) =>{
-    console.log(`data recieved user ${name} with id: ${id}`)
+stream.on('data', (result)=>{
+    console.log(result)
 })
-
-customEmitter.on('response', () =>{
-    console.log(`some other logic`)
-})
-
-customEmitter.emit('response', 'john', 34)
