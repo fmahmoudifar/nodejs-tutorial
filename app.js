92045d34bf91fc4a67520207c2421c0748ebdf19
fmahmoudifar <fmahmoudifar@gmail.com>
2022-08-26 09:48:08 +0300

    1 file modified, 1 deleted

--- app.js
+++ app.js
@@ -1,7 +1,4 @@
-const { createReadStream } = require('fs')
-
-const stream = createReadStream('./content/big.txt')
-
-stream.on('data', (result)=>{
-    console.log(result)
-})
+const { writeFileSync } = require('fs')
+for (let i = 0; i < 100000; i++) {
+  writeFileSync('./content/big.txt', `hello world ${i}\n`, { flag: 'a' })
+}
