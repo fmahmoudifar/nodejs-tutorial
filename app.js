80c412a7e64f5471fa5beecfd0eee32f70ae30e4
fmahmoudifar <fmahmoudifar@gmail.com>
2022-08-28 10:13:43 +0300

    1 file modified

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log('testrrr');
+// console.log('testrrr');
+
+const EventEmitter = require('events');
+
+const customEmitter = new EventEmitter();
+
+customEmitter.on('response', (name, id) => {
+  console.log(`data recieved user ${name} with id: ${id}`);
+});
+
+customEmitter.on('response', () => {
+  console.log(`some other logic`);
+});
+
+customEmitter.emit('response', 'john', 34);
